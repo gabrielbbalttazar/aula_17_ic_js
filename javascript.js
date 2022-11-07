b4cdd43c6cf6acf8db5b41593c29a1f935f7ceb9
gabrielbbalttazar <gabriel_baltazar@hotmai.com>
2022-11-07 20:58:40 -0300

    commit

---
+++ javascript.js
@@ -0,0 +1,38 @@
+var n = 1;
+console.log (n);
+console.log (typeof(n));
+
+var texto = "teste";
+console.log (texto);
+console.log (typeof(texto));
+
+var n1 = 10;
+var n2 = 15;
+var n3 = 20;
+var media = (n1 + n2 + n3) / 3;
+console.log ("5) A média entre " + n1 + " " +  " " + n2 + " " + n3 + " é igual a " + media);
+
+var ap1 = 1.5;
+var ap2 = 2.5;
+var as = 6;
+var media_final = ap1 + ap2 + as;
+console.log ("6) A média final é igual a " + media_final);
+
+var nome = "João";
+var idade = 17;
+if (idade >= 18) {
+    console.log ("7)" + nome + ", você já é maior de idade!");
+} 
+else {
+    console.log ("7)" +nome + ", você ainda NÃO é maior de idade!");
+}
+
+var num1 = 15;
+var num2 = 18;
+var num3 = 13;
+var maior = num1;
+if (num2 > maior);
+    maior = num2;
+if (num3 > maior);
+    maior = num3;
+console.log ("8) O maior numero é: " + maior);
